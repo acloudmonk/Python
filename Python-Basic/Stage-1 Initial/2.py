027c8034947d0850a6f7f81f7c6ebad55193b90c
Sachin Singh <49566909+sachinsingh2005@users.noreply.github.com>
2023-12-02 10:44:57 +0530

    fixes

--- Python-Basic/Stage-1 Initial/2.py
+++ Python-Basic/Stage-1 Initial/2.py
@@ -3,7 +3,7 @@ x = 5
 y = 10
 print("The sum of", x, "and", y, "is", x + y)
 
-# Store input numbers
+print("##### Using Input function #####")
 x = input("Type a number: ")
 y = input("Type another number: ")
 # Add two numbers. input returns a string and we need to covert it to either int or float based on our need
@@ -11,17 +11,10 @@ sum = int(x) + int(y)
 # Display the sum
 print("The sum of {0} and {1} is {2}".format(x, y, sum))
 
+
 # All operations in Single line. Although this program uses no variable (memory efficient), it is harder to read.
+print("##### Use all statement in single line #####")
 print(
     "The sum is %.1f"
     % (float(input("Enter first number: ")) + float(input("Enter second number: ")))
 )
-
-# Python program to add two numbers using lambda
-# Driver Code
-num1 = 15
-num2 = 12
-# Adding two numbers
-sum_twoNum = lambda num1, num2: num1 + num2
-# printing values
-print("Sum of {0} and {1} is {2};".format(num1, num2, sum_twoNum(num1, num2)))
--- Python-Basic/Stage-1 Initial/9.py
+++ Python-Basic/Stage-1 Initial/9.py
@@ -1,17 +1,9 @@
 # there are different ways to swap two variables in Python. Here are a few examples:
 
-# Here's the Python code to swap two numbers:
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
-print("Before swapping: a =", a, "and b =", b)
-# swap the values
-a, b = b, a
-print("After swapping: a =", a, "and b =", b)
 
-
-# Using a temporary variable:
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
+print("############## Using a temporary variable ########################")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 print("Before swapping: a =", a, "and b =", b)
 # swap the values using a temporary variable
 temp = a
@@ -20,9 +12,9 @@ b = temp
 print("After swapping: a =", a, "and b =", b)
 
 
-# Using arithmetic operations:
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
+print("############## Using arithmetic operations #######################")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 print("Before swapping: a =", a, "and b =", b)
 # swap the values using arithmetic operations
 a = a + b
@@ -31,12 +23,36 @@ a = a - b
 print("After swapping: a =", a, "and b =", b)
 
 
-# Using bitwise XOR operator:
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
+print("############## Using bitwise XOR operator ########################")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 print("Before swapping: a =", a, "and b =", b)
 # swap the values using bitwise XOR operator
 a = a ^ b
 b = a ^ b
 a = a ^ b
 print("After swapping: a =", a, "and b =", b)
+
+
+print("############## Using tuple swapping ##############################")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("Before swapping: a =", a, "and b =", b)
+# swap the values
+a, b = b, a
+print("After swapping: a =", a, "and b =", b)
+
+"""
+The statement "a, b = b, a" in Python is called a tuple unpacking or tuple swapping.
+The right-hand side creates a tuple (b, a), and the left-hand side unpacks the values
+from this tuple into the variables a and b.
+This results in the values of a and b being swapped.1
+"""
+
+print("############## tuple swapping with 4 variable ####################")
+a = 10
+b = 20
+c = 30
+d = 40
+a, b, c, d = d, c, b, a
+print("a =", a, "\nb =", b, "\nc =", c, "\nd =", d)
